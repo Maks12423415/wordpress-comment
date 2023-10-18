d2aef4acb55cca4f8fc9ffb7c4fddec5575472f6
Maks12423415 <126457937+Maks12423415@users.noreply.github.com>
2023-10-18 10:05:56 +0200

    .

--- klient/app.js
+++ klient/app.js
@@ -6,12 +6,19 @@ async function getData() {
 
   for (let i in comments) {
     const comment = comments[i];
-
     const div = document.createElement("div");
     div.classList.add("divy");
-
     const p = document.createElement("p");
     p.innerHTML = comment.content.rendered;
+
+    if (comment.post && comment.post !== 0) {
+      const post = await fetchPost(comment.post);
+      if (post) {
+        const h1 = document.createElement("h1");
+        h1.innerHTML = post.title.rendered;
+        div.appendChild(h1);
+      }
+    }
 
     if (
       comment.content.rendered.includes("kupa") ||
@@ -23,18 +30,16 @@ async function getData() {
       button.addEventListener("click", () => {
         changeStatus(comment.id);
       });
+
+      const button2 = document.createElement("button");
+      button2.innerHTML = "Dopowiedz";
+      button2.addEventListener("click", () => {
+        odp(comment.id);
+      });
+      div.appendChild(button2);
       div.appendChild(button);
     } else {
       console.log("jest git");
-    }
-
-    if (comment.post && comment.post !== 0) {
-      const post = await fetchPost(comment.post);
-      if (post) {
-        const h1 = document.createElement("h1");
-        h1.innerHTML = post.title.rendered;
-        div.appendChild(h1);
-      }
     }
 
     div.appendChild(p);
@@ -60,7 +65,7 @@ async function changeStatus(commentId) {
   const url = new URL(
     `http://localhost/wordpress/wp-json/wp/v2/comments/${commentId}`
   );
-  await fetch(url, {
+  const data2 = await fetch(url, {
     method: "DELETE",
     headers: {
       authorization: `Basic ${btoa("Maks:zaq12wsx")}`,
@@ -70,3 +75,22 @@ async function changeStatus(commentId) {
 }
 
 getData();
+
+async function odp(id) {
+  const url3 = new URL(
+    `http://localhost/wordpress/wp-json/wp/v2/comments/${id}`
+  );
+  const tresc = prompt("Wprowadź tresc odpowiedzi");
+  const data3 = await fetch(url3, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Basic ${btoa("Maks:zaq12wsx")}`,
+    },
+    body: JSON.stringify({
+      post: id,
+      content: tresc,
+    }),
+  });
+  //location.reload();
+}
